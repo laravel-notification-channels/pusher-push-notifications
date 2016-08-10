85057dc28d81db2ce8bed363e1b96e17214c852a
Mohamed Said <themohamedsaid@gmail.com>
2016-08-10 23:12:16 +0200

    wording, some style fixes

--- src/Channel.php
+++ src/Channel.php
@@ -31,6 +31,8 @@ class Channel
      *
      * @param mixed $notifiable
      * @param \Illuminate\Notifications\Notification $notification
+     *
+     * @return void
      */
     public function send($notifiable, Notification $notification)
     {
